b1e5cf839e82e456819780bcd461bd5b1c23ca5d
Pedro Diaz <pedro.diaz@playvrtoday.com>
2018-02-13 20:57:16 -0800

    Initial commit for WPF app

---
+++ SampleWordApp/ViewModel/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SampleWordApp.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ SampleWordApp/ViewModel/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace SampleWordApp.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private Action _action;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
